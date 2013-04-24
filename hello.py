da59009cd3acf8a37f8f10fd98fdfd966d89b047
cloudaice <cloudaice@163.com>
2013-04-24 14:16:51 +0200

    add favicon

--- hello.py
+++ hello.py
@@ -1,16 +1,25 @@
 import tornado.ioloop
 import tornado.web
+import os
+from tornado.options import parse_command_line
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("hello world!")
+        self.write("Hello world!")
+
+
+settings = {
+    "static_path": os.path.join(os.path.dirname(__file__), 'static'),
+}
 
 application = tornado.web.Application([
     (r"/", MainHandler),
-])
+    (r"/favicon.ico", tornado.web.StaticFileHandler, dict(path=settings["static_path"])),
+], **settings)
 
 
 if __name__ == "__main__":
+    parse_command_line()
     application.listen(18888)
     tornado.ioloop.IOLoop.instance().start()
