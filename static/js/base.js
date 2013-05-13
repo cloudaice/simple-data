c70ac4fd25dacea67bc9cc62af00b9f58525717d
cloudaice <cloudaice@163.com>
2013-05-13 22:53:18 +0200

    make base.js can get data and show tables

--- static/js/base.js
+++ static/js/base.js
@@ -1,18 +1,28 @@
 $(document).ready(function(){
     url = window.location.href;
-    //$('#topTab').children().removeClass('active');
     lasturl = url.split('/');
     classname = '/' + lasturl[lasturl.length-1]; 
-    if (classname == '/classroom'){
-        $('#top1').addClass('active');
-        console.debug(classname);
-    }
-    if (classname == '/curriculum'){
-        $('#top2').addClass('active');
-        console.debug(classname);
-    }
-    if (classname == '/about'){
-        $('#top3').addClass('active');
-        console.debug(classname);
+    load_table();
+    function load_table(){
+        console.debug("hello");
+        $.ajax({
+            type: 'POST',
+            url: '/github',
+            data: {"site": "Tornado-data"},
+            dataType: "json",
+            success: function(data){
+                console.debug("hello");
+                var github_table  = "<table class='table table-striped'><tr>";
+                for (var i in data){
+                    github_table += "<td class='solid'>#" + i + "</td>";
+                    github_table += "<td class='solid'>" + data[i]["name"] + "</td>";
+                    github_table += "<td class='solid'>" + data[i]["location"] + "</td>";
+                    github_table += "<td class='solid'>" + "<img height='48' width='48' src=" + data[i]["gravatar"] + "/>" + "</td>";
+                    github_table +="</tr><tr>";
+                }
+                github_table += "</tr><table>";
+                $("#github-table").html(github_table);
+            } 
+        });
     }
 });
