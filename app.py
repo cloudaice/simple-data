f026a0b97241ccdd4985c2498e1ddfd78e53fa7b
cloudaice <cloudaice@163.com>
2013-05-13 23:49:21 +0200

    add wrap loop instance to fetch background

--- app.py
+++ app.py
@@ -4,16 +4,54 @@ import os
 import json
 from tornado import escape
 import tornado.ioloop
-import config
 from tornado import web
 from tornado import gen
 from tornado import httpclient
 from tornado.web import asynchronous
 from tornado.options import parse_command_line, options, define
+import config
+from functools import wraps
+import datetime
 
 
+github_data = {}
 define("port", default=8000)
 strong = lambda x: 2 ** 11 / (1 + pow(exp(1), -(x - 2 ** 8) / 2 ** 6))
+
+
+def loop_call(delta=10 * 1000):
+    def wrap_loop(func):
+        @wraps(func)
+        def wrap_func(*args, **kwargs):
+            func(*args, **kwargs)
+            tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(milliseconds=delta), wrap_func)
+        return wrap_func
+    return wrap_loop
+
+
+@loop_call(5 * 1000)
+@gen.coroutine
+def get_raw_data():
+    """
+    every 5 seconds will fetch github.com
+    """
+    global github_data
+    client = httpclient.AsyncHTTPClient()
+    request = TornadoDataRequest("https://api.github.com/gists/4524946")
+    resp = yield client.fetch(request)
+    resp = escape.json_decode(resp.body)
+    users_url = resp["files"]["github-users-stats.json"]["raw_url"]
+    languages_url = resp["files"]["github-languages-stats.json"]["raw_url"]
+    users, languages = yield [client.fetch(TornadoDataRequest(users_url)),
+                              client.fetch(TornadoDataRequest(languages_url))]
+    users_stats = escape.json_decode(users.body)
+    languages_stats = escape.json_decode(languages.body)
+    print len(users_stats)
+    print len(languages_stats)
+    users_stats = sorted(users_stats, key=lambda d: d["contributions"] + strong(d["followers"]), reverse=True)
+    users_stats = filter(lambda u: 'china' in u['location'].lower(), users_stats)
+    github_data["users_stats"] = users_stats
+    github_data["languages_stats"] = languages_stats
 
 
 class TornadoDataRequest(httpclient.HTTPRequest):
@@ -26,16 +64,19 @@ class TornadoDataRequest(httpclient.HTTPRequest):
 
 
 class MainHandler(web.RequestHandler):
+    @asynchronous
     def get(self):
         self.render("index.html")
 
 
 class HelloHandler(web.RequestHandler):
+    @asynchronous
     def get(self):
         self.write("Hello world!")
 
 
 class LoveHandler(web.RequestHandler):
+    @asynchronous
     def get(self):
         self.write("Love world!")
 
@@ -55,23 +96,11 @@ class GithubHandler(web.RequestHandler):
     @asynchronous
     @gen.coroutine
     def post(self):
-        client = httpclient.AsyncHTTPClient()
-        request = TornadoDataRequest("https://api.github.com/gists/4524946")
-        resp = yield client.fetch(request)
-        resp = escape.json_decode(resp.body)
-        users_url = resp["files"]["github-users-stats.json"]["raw_url"]
-        languages_url = resp["files"]["github-languages-stats.json"]["raw_url"]
-        users, languages = yield [client.fetch(TornadoDataRequest(users_url)),
-                                  client.fetch(TornadoDataRequest(languages_url))]
-        users_stats = escape.json_decode(users.body)
-        languages_stats = escape.json_decode(languages.body)
-        print len(users_stats)
-        print len(languages_stats)
-        users_stats = sorted(users_stats, key=lambda d: d["contributions"] + strong(d["followers"]), reverse=True)
-        users_stats = filter(lambda u: 'china' in u['location'].lower(), users_stats)
+        global github_data
+        users_stats = github_data["users_stats"]
+        #languages_stats = github_data["languages_stats"]
         self.set_header('Content-Type', 'application/json; charset=UTF-8')
         self.write(json.dumps(users_stats, indent=4, separators=(',', ': ')))
-        #self.write(json.dumps(languages_stats, indent=4, separators=(',', ': ')))
         self.finish()
 
 
@@ -93,4 +122,5 @@ app = web.Application([
 if __name__ == "__main__":
     parse_command_line()
     app.listen(options.port)
+    get_raw_data()
     tornado.ioloop.IOLoop.instance().start()
