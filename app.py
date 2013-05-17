f8f589a0e1c7de9198bc69f72689d641561edee2
cloudaice <cloudaice@163.com>
2013-05-17 10:52:57 +0200

    use PutPage and GetPage instead and add more log info

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 #-*-coding: utf-8-*-
 from math import exp
 import os
+import time
 import json
 import datetime
 import base64
-import logging
+import toro
 from tornado import escape
 from tornado import web
 from tornado import gen
 from tornado import httpclient
-from tornado.httpclient import HTTPError
+#from tornado.httpclient import HTTPError
 from tornado.httpserver import HTTPServer
 from tornado.web import asynchronous
 from tornado.options import parse_command_line, options, parse_config_file
@@ -17,62 +18,87 @@ from functools import wraps
 import tornado.ioloop
 import tornado.log
 from addr import searchpage
-#import config
+from libs.client import GetPage, PutPage
 
 
 github_data = {}
 parse_config_file("config.py")
 
 formula = lambda x: 2 ** 11 / (1 + pow(exp(1), -(x - 2 ** 8) / 2 ** 6))
-logger = logging.getLogger("tornado-data")
+event = toro.Event
 
 
 def loop_call(delta=60 * 1000):
     def wrap_loop(func):
         @wraps(func)
+        @gen.coroutine
         def wrap_func(*args, **kwargs):
-            func(*args, **kwargs)
+            try:
+                yield func(*args, **kwargs)
+            except:
+                options.logger.error("fetch error in loop_call")
+            options.logger.info("process end %d" % int(time.time()))
             tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(milliseconds=delta), wrap_func)
         return wrap_func
     return wrap_loop
 
-    
+
 @loop_call(5 * 1000)
 @gen.coroutine
 def get_raw_data():
     """
-    every 5 seconds will fetch github.com
+    Every 5 seconds will fetch github.com
     """
+    options.logger.info("start fetch %d" % int(time.time()))
     global github_data
-    httpclient.AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
-    client = httpclient.AsyncHTTPClient()
-    request = TornadoDataRequest("https://api.github.com/gists/4524946")
-    resp = yield client.fetch(request)
-    resp = escape.json_decode(resp.body)
-    users_url = resp["files"]["github-users-stats.json"]["raw_url"]
-    languages_url = resp["files"]["github-languages-stats.json"]["raw_url"]
-    users, languages = yield [client.fetch(TornadoDataRequest(users_url)),
-                              client.fetch(TornadoDataRequest(languages_url))]
-    users_stats = escape.json_decode(users.body)
-    languages_stats = escape.json_decode(languages.body)
-    users_stats = sorted(users_stats, key=lambda d: d["contributions"] + formula(d["followers"]), reverse=True)
-    users_stats = filter(lambda u: 'china' in u['location'].lower(), users_stats)
-    github_data["users_stats"] = users_stats
-    github_data["languages_stats"] = languages_stats
+    resp = yield GetPage("https://api.github.com/gists/4524946")
+    if resp.code == 200:
+        print resp.headers["X-RateLimit-Limit"]
+        print resp.headers["X-RateLimit-Remaining"]
+        options.logger.info("fetch gists sunccess")
+        resp = escape.json_decode(resp.body)
+        users_url = resp["files"]["github-users-stats.json"]["raw_url"]
+        languages_url = resp["files"]["github-languages-stats.json"]["raw_url"]
+        users, languages = yield [GetPage(users_url),
+                                  GetPage(languages_url)]
+        if users.code == 200 and languages.code == 200:
+            if "X-RateLimit-Remaining" in languages.headers:
+                print languages.headers["X-RateLimit-Remaining"]
+            if "X-RateLimit-Remaining" in users.headers:
+                print users.headers["X-RateLimit-Remaining"]
+            users_stats = escape.json_decode(users.body)
+            languages_stats = escape.json_decode(languages.body)
+            users_stats = sorted(users_stats,
+                                 key=lambda d: d["contributions"] + formula(d["followers"]),
+                                 reverse=True)
+            users_stats = filter(lambda u: 'china' in u['location'].lower(), users_stats)
+            github_data["users_stats"] = users_stats
+            github_data["languages_stats"] = languages_stats
+            options.logger.info("fetch users and languages success")
+        else:
+            options.logger.error("%d, %r" % (users.code, users.message))
+            options.logger.error("%d, %r" % (languages.code, languages.message))
+    else:
+        options.logger.error("%d, %r" % (resp.code, resp.message))
+    gen.Return()
+    #tornado.ioloop.IOLoop.instance().add_timeout(
+    #    datetime.timedelta(milliseconds=5 * 1000),
+    #    get_raw_data)
 
 
-class BaseHandler(web.RequestHandler):
+class ApiHandler(web.RequestHandler):
     def __init__(self, *args, **kwargs):
-        super(BaseHandler, self).__init__(*args, **kwargs)
-
+        super(ApiHandler, self).__init__(*args, **kwargs)
+        super(ApiHandler, self).set_header('Content-Type', 'application/json; charset=UTF-8')
+        
     def prepare(self):
         """do something before request comming"""
-        #logger.debug(self.request)
+        #options.logger.debug(self.request)
         pass
 
     def on_finish(self):
         """do something after response to client like logging"""
-        #logger.debug("finish request.")
+        #options.logger.debug("finish request.")
         pass
 
 
@@ -85,12 +111,83 @@ class TornadoDataRequest(httpclient.HTTPRequest):
         self.user_agent = "Tornado-data"
 
 
-@gen.coroutine
-def GetPage(url):
-    client = httpclient.AsyncHTTPClient()
-    request = TornadoDataRequest(url)
-    response = yield client.fetch(request)
-    raise gen.Return(response)
+class GithubPageHandler(web.RequestHandler):
+    @asynchronous
+    @gen.coroutine
+    def get(self):
+        resp = yield GetPage(searchpage(2))
+        if resp.code == 200:
+            self.write(resp.body)
+        else:
+            self.write("%d, %r" % (resp.code, resp.message))
+        self.finish()
+
+
+class FetchUserHandler(ApiHandler):
+    @asynchronous
+    @gen.coroutine
+    def get(self):
+        resp = yield GetPage(options.contribution_url("cloudaice"))
+        if resp.code == 200:
+            resp = escape.json_decode(resp.body)
+            self.write(json.dumps(resp, indent=4, separators=(",", ": ")))
+        else:
+            self.write("%d, %r" % (resp.code, resp.message))
+        self.finish()
+
+
+class GithubCiHandler(ApiHandler):
+    @asynchronous
+    @gen.coroutine
+    def get(self):
+        global remote_users_file
+        resp = yield GetPage(options.api_url + "/repos/cloudaice/simple-data/contents/test.md")
+        if resp.code == 404:
+            options.logger.info("fetch file 404")
+            self.write("%d, %r" % (resp.code, resp.message))
+        elif resp.code == 200:
+            resp = escape.json_decode(resp.body)
+            sha = resp['sha']
+            url = options.api_url + "/repos/cloudaice/simple-data/contents/test.md"
+            body = json.dumps({
+                "message": "update users.json",
+                "content": base64.b64encode(
+                    json.dumps(
+                        {"name": "cloudaice", "type": "test"},
+                        indent=4,
+                        separators=(',', ': ')
+                    )
+                ),
+                "committer": {"name": "cloudaice", "email": "cloudaice@163.com"},
+                "sha": sha
+            })
+            resp = yield PutPage(url, body)
+            if resp.code == 200:
+                resp = escape.json_decode(resp.body)
+                if isinstance(resp, dict):
+                    self.write(json.dumps(resp, indent=4, separators=(',', ': ')))
+                    options.logger.info(json.dumps(resp, indent=4, separators=(',', ': ')))
+                else:
+                    self.write("Failed")
+            else:
+                self.write("%d, %r" % (resp.code, resp.message))
+        else:
+            self.write(resp.message)
+        self.finish()
+
+
+class GithubHandler(ApiHandler):
+    @asynchronous
+    @gen.coroutine
+    def post(self):
+        global github_data
+        if 'users_stats' not in github_data:
+            users_stats = []
+        else:
+            users_stats = github_data["users_stats"]
+        #languages_stats = github_data["languages_stats"]
+        self.write(json.dumps(users_stats, indent=4, separators=(',', ': ')))
+        self.finish()
 
 
 class MainHandler(web.RequestHandler):
@@ -105,104 +202,6 @@ class AboutHandler(web.RequestHandler):
         self.render("about.html")
         
 
-class HelloHandler(web.RequestHandler):
-    @asynchronous
-    def get(self):
-        self.write("Hello world!")
-
-
-class LoveHandler(web.RequestHandler):
-    @asynchronous
-    def get(self):
-        self.write("Love world!")
-
-
-class GithubPageHandler(web.RequestHandler):
-    @asynchronous
-    @gen.coroutine
-    def get(self):
-        client = httpclient.AsyncHTTPClient()
-        request = TornadoDataRequest(searchpage(2))
-        resp = yield client.fetch(request)
-        self.write(resp.body)
-        self.finish()
-
-
-class FetchUserHandler(web.RequestHandler):
-    @asynchronous
-    @gen.coroutine
-    def get(self):
-        client = httpclient.AsyncHTTPClient()
-        request = TornadoDataRequest("https://github.com/users/cloudaice/contributions_calendar_data")
-        resp = yield client.fetch(request)
-        self.write(resp.body)
-        self.finish()
-
-
-class GithubCiHandler(web.RequestHandler):
-    @asynchronous
-    @gen.coroutine
-    def get(self):
-        global remote_users_file
-        #client = httpclient.AsyncHTTPClient()
-        #request = TornadoDataRequest("https://api.github.com/repos/cloudaice/simple-data/contents/test1.md")
-        #future = client.fetch(request)
-        try:
-            resp = yield GetPage("https://api.github.com/repos/cloudaice/simple-data/contents/test1.md")
-        except HTTPError, e:
-            if e.code == "404":
-                pass  # Create this file
-            else:
-                raise e
-        resp = escape.json_decode(resp.body)
-        sha = resp['sha']
-        client = httpclient.AsyncHTTPClient()
-        request = httpclient.HTTPRequest(
-            "https://api.github.com/repos/cloudaice/simple-data/contents/test.md",
-            method="PUT",
-            headers={'Content-Type': 'application/json; charset=UTF-8'},
-            auth_username=options.username,
-            auth_password=options.password,
-            user_agent="Tornado-Data",
-            body=json.dumps({
-                "message": "update users.json",
-                "content": base64.b64encode(
-                    json.dumps(
-                        {"name": "cloudaice", "type": "test"},
-                        indent=4,
-                        separators=(',', ': ')
-                    )
-                ),
-                "committer": {"name": "cloudaice", "email": "cloudaice@163.com"},
-                "sha": sha
-            })
-        )
-        resp = yield client.fetch(request)
-        if resp.error:
-            print resp.error
-        resp = escape.json_decode(resp.body)
-        if isinstance(resp, dict):
-            self.write(json.dumps(resp, indent=4, separators=(',', ': ')))
-        else:
-            self.write("Failed")
-        self.finish()
-
-
-class GithubHandler(web.RequestHandler):
-    @asynchronous
-    @gen.coroutine
-    def post(self):
-        global github_data
-        if 'users_stats' not in github_data:
-            users_stats = []
-        else:
-            users_stats = github_data["users_stats"]
-        #languages_stats = github_data["languages_stats"]
-        self.set_header('Content-Type', 'application/json; charset=UTF-8')
-        self.write(json.dumps(users_stats, indent=4, separators=(',', ': ')))
-        self.finish()
-
-
 class Application(web.Application):
     def __init__(self, *arhs):
         settings = {
@@ -214,7 +213,6 @@ class Application(web.Application):
         handlers = [
             (r"/", MainHandler),
             (r"/about", AboutHandler),
-            (r"/love", LoveHandler),
             (r"/github", GithubHandler),
             (r"/githubpage", GithubPageHandler),
             (r"/githubci", GithubCiHandler),
@@ -227,12 +225,7 @@ class Application(web.Application):
 
 if __name__ == "__main__":
     parse_command_line()
-    logger.debug("debug")
-    logger.error("error")
-    logger.info("info")
-    logger.warning("warning")
     get_raw_data()
-
     app = HTTPServer(Application())
     app.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
