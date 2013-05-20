7edb9c63e43c27ac25f456a1cc04055074643e87
cloudaice <cloudaice@163.com>
2013-05-20 23:31:32 +0200

    add map post data

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from tornado.web import asynchronous
 from tornado.options import parse_command_line, options, parse_config_file
 import tornado.ioloop
 import tornado.log
-from addr import searchpage
 from libs.client import GetPage, sync_loop_call, formula
 import workers
 
@@ -18,12 +17,39 @@ import workers
 github_data = {}
 parse_config_file("config.py")
 
-city_list = ["heilongjiang", "jilin", "liaoning", "hebei", "shandong", "jiangsu",
-             "zhejiang", "anhui", "henan", "shanxi", "shanxii", "gansu", "hubei",
-             "jiangxi", "fujian", "hunan", "guizou", "sichuan", "yunnan", "qinghai",
-             "hainan", "shanghai", "chongqing", "tianjin", "beijing", "ningxia",
-             "neimenggu", "guangxi", "xinjiang", "xizang", "guangdong",
-             "xianggang", "taiwan", "aomen"]
+city_list = [
+    "heilongjiang", "jilin", "liaoning", "hebei", "shandong", "jiangsu", "zhejiang", "anhui",
+    "henan", "shanxi", "shanxii", "gansu", "hubei", "jiangxi", "fujian", "hunan", "guizou",
+    "sichuan", "yunnan", "qinghai", "hainan", "shanghai", "chongqing", "tianjin", "beijing", "ningxia",
+    "neimenggu", "guangxi", "xinjiang", "xizang", "guangdong", "xianggang", "taiwan", "aomen"]
+
+country_list = [
+    'United Arab Emirates', 'Afghanistan', 'Albania', 'Armenia', 'Angola', 'Argentina', 'Austria',
+    'Australia', 'Azerbaijan', 'Bosnia and Herzegovina', 'Bangladesh', 'Belgium', 'Burkina Faso',
+    'Bulgaria', 'Burundi', 'Benin', 'Brunei Darussalam', 'Plurinational State of Bolivia', 'Brazil',
+    'Bhutan', 'Botswana', 'Belarus', 'Belize', 'Canada', 'The Democratic Republic of the Congo',
+    'Central African Republic', 'Congo', 'Switzerland', 'Ivory Coast', 'Chile', 'Cameroon', 'China',
+    'Colombia', 'Costa Rica', 'Cuba', 'Cyprus', 'Czech Republic', 'Germany', 'Djibouti', 'Denmark',
+    'Dominican Republic', 'Algeria', 'Ecuador', 'Estonia', 'Egypt', 'Western Sahara', 'Eritrea',
+    'Spain', 'Ethiopia', 'Finland', 'Fiji', 'Falkland Islands (Malvinas)', 'France', 'Gabon',
+    'United Kingdom', 'Georgia', 'French Guiana', 'Ghana', 'Greenland', 'Gambia', 'Guinea',
+    'Equatorial Guinea', 'Greece', 'Guatemala', 'Guinea-Bissau', 'Guyana', 'Honduras', 'Croatia',
+    'Haiti', 'Hungary', 'Indonesia', 'Ireland', 'Israel', 'India', 'Iraq', 'Islamic Republic of Iran',
+    'Iceland', 'Italy', 'Jamaica', 'Jordan', 'Japan', 'Kenya', 'Kyrgyzstan', 'Cambodia',
+    'Democratic People\u2019s Republic of Korea', 'Republic of Korea', 'Kuwait', 'Kazakhstan',
+    'Lao People\u2019s Democratic Republic', 'Lebanon', 'Sri Lanka', 'Liberia', 'Lesotho', 'Lithuania',
+    'Luxembourg', 'Latvia', 'Libyan Arab Jamahiriya', 'Morocco', 'Republic of Moldova', 'Madagascar',
+    'The Former Yugoslav Republic of Macedonia', 'Mali', 'Myanmar', 'Mongolia', 'Mauritania', 'Malawi',
+    'Mexico', 'Malaysia', 'Mozambique', 'Namibia', 'New Caledonia', 'Niger', 'Nigeria', 'Nicaragua',
+    'Netherlands', 'Norway', 'Nepal', 'New Zealand', 'Oman', 'Panama', 'Peru', 'Papua New Guinea',
+    'Philippines', 'Pakistan', 'Poland', 'Puerto Rico', 'Occupied Palestinian Territory', 'Portugal',
+    'Paraguay', 'Qatar', 'Romania', 'Serbia', 'Russian Federation', 'Rwanda', 'Saudi Arabia',
+    'Solomon Islands', 'Sudan', 'Sweden', 'Svalbard and Jan Mayen', 'Slovakia', 'Sierra Leone',
+    'Senegal', 'Somalia', 'Suriname', 'El Salvador', 'Syrian Arab Republic', 'Swaziland', 'Chad',
+    'Togo', 'Thailand', 'Tajikistan', 'Timor-Leste', 'Turkmenistan', 'Tunisia', 'Turkey',
+    'Province of China Taiwan', 'United Republic of Tanzania', 'Ukraine', 'Uganda', 'United States',
+    'Uruguay', 'Uzbekistan', 'Bolivarian Republic of Venezuela', 'Viet Nam', 'Vanuatu', 'Yemen',
+    'South Africa', 'Zambia', 'Zimbabwe']
 
 
 @sync_loop_call(60 * 1000)
@@ -89,16 +115,72 @@ class TornadoDataRequest(httpclient.HTTPRequest):
         self.user_agent = "Tornado-data"
 
 
-class GithubPageHandler(web.RequestHandler):
+class ChinaMapHandler(ApiHandler):
     @asynchronous
     @gen.coroutine
-    def get(self):
-        resp = yield GetPage(searchpage(2))
-        if resp.code == 200:
-            self.write(resp.body)
-        else:
-            self.write("%d, %r" % (resp.code, resp.message))
-        self.finish()
+    def post(self):
+        china_map = {}
+        for city in city_list:
+            china_map[city] = {"score": 0, "stateInitColor": 6}
+
+        for user in workers.github_china:
+            try:
+                location = user["location"].lower()
+            except Exception, e:
+                options.logger.error("location error: %s" % e)
+                continue
+            for city in city_list:
+                if "hangzhou" in location:
+                    china_map["zhejiang"]["score"] += 1
+                    break
+                if "harbin" in location:
+                    china_map['heilongjiang']["score"] += 1
+                    break
+                if city in location:
+                    china_map[city]['score'] += 1
+                    break
+
+        top_score = max([city["score"] for cityname, city in china_map.items()])
+        capture = top_score / 6
+        if capture == 0:
+            capture = 1
+        for city in china_map:
+            china_map[city]['stateInitColor'] = 6 - china_map[city]['score'] / capture
+            if china_map[city]['score'] > 0 and china_map[city]['score'] < 5:
+                china_map[city]['stateInitColor'] = 5
+            elif china_map[city]['score'] >= 5 and china_map[city]['score'] < 10:
+                china_map[city]['stateInitColor'] = 4
+            elif china_map[city]['score'] >= 10 and china_map[city]['score'] < 50:
+                china_map[city]['stateInitColor'] = 3
+
+        self.write(json.dumps(china_map, indent=4, separators=(',', ': ')))
+
+                
+class WorldMapHandler(ApiHandler):
+    @asynchronous
+    @gen.coroutine
+    def post(self):
+        world_map = {}
+        for country in country_list:
+            world_map[country] = {"score": 0, "stateInitColor": 6}
+        for user in workers.github_world:
+            try:
+                location = user["location"].lower()
+            except Exception, e:
+                options.logger.error("location error: %s" % e)
+                continue
+            for country in country_list:
+                if country in location:
+                    world_map[country]["score"] += 1
+                    break
+        top_score = max([world_map[country]["score"] for country in world_map])
+        capture = top_score / 6
+        if capture == 0:
+            capture = 1
+        for country in world_map:
+            world_map[country]["stateInitColor"] = 6 - world_map[country]["score"] / capture
+        
+        self.write(json.dumps(world_map, indent=4, separators=(',', ': ')))
 
 
 class FetchUserHandler(ApiHandler):
@@ -159,22 +241,23 @@ class AboutHandler(web.RequestHandler):
 settings = {
     "static_path": os.path.join(os.path.dirname(__file__), 'static'),
     'template_path': os.path.join(os.path.dirname(__file__), 'template'),
-    "debug": True
+    "debug": False
 }
 
 handlers = [
     (r"/", MainHandler),
     (r"/github", GithubHandler),
-    (r"/githubpage", GithubPageHandler),
     (r"/githubchina", GithubChinaHandler),
     (r"/githubworld", GithubWorldHandler),
+    (r"/chinamap", ChinaMapHandler),
+    (r"/worldmap", WorldMapHandler),
     (r"/user", FetchUserHandler),
     (r"/about", AboutHandler),
     (r"/favicon.ico", web.StaticFileHandler, dict(path=settings["static_path"])),
 ]
 
 app = web.Application(handlers, **settings)
-get_raw_data()
+#get_raw_data()
 workers.update_china_user()
 workers.update_world_user()
 
