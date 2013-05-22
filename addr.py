e4f53c19b7510ca4128e30e7a291bcf8c7239a9b
cloudaice <cloudaice@163.com>
2013-05-22 21:49:37 +0200

    rm addr file

--- addr.py
+++
@@ -1,7 +0,0 @@
-#-*-coding: utf-8-*-
-
-def searchpage(p):
-    if p == 1:
-        return "https://github.com/search?q=location:china&s=followers&type=Users"
-    else:
-        return "https://github.com/search?p=" + str(p) + "&q=location:china&s=followers&type=Users"
