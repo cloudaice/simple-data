566ed9c11f963dd194824489713c59bc67599d8b
cloudaice <cloudaice@163.com>
2013-05-28 14:01:18 +0200

    add auth_username info in fetch hreader

--- libs/geo.py
+++ libs/geo.py
@@ -1,8 +1,7 @@
 #-*-coding: utf-8-*-
 
-from tornado.httpclient import AsyncHTTPClient, HTTPError
+from tornado.httpclient import AsyncHTTPClient, HTTPError, HTTPRequest
 from tornado.options import options
-import workers
 from tornado import gen
 from tornado import escape
 
@@ -10,42 +9,36 @@ from tornado import escape
 AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
 
 
+class GeoRequest(HTTPRequest):
+    def __init__(self, url, **kwargs):
+        super(GeoRequest, self).__init__(url, **kwargs)
+        self.user_agent = "Tornado-data"
+        self.auth_username = options.username
+
+
 @gen.coroutine
 def GeoFetch(keyword):
     client = AsyncHTTPClient()
-    url = "view-source:http://api.geonames.org/searchJSON?q=Xiaoshan,%20Zhejiang,%20China&maxRows=10&username=" + options.username
+    url = "http://api.geonames.org/searchJSON?q=" + keyword + "&maxRows=10&username=" + options.username
+    request = GeoRequest(url)
     try:
-        resp = yield client.fetch(url)
+        resp = yield client.fetch(request)
     except HTTPError, e:
         options.logger.error("fetch geoname error %d %s" % (e.code, e.message))
         resp = e
     raise gen.Return(resp)
 
 
-@gen.coroutine
-def collect_geoname():
-    china_map = {}
-    for city in options.city_list:
-        china_map[city] = {"score": 0, "stateInitColor": 6}
-    for user in workers.github_china:
-        try:
-            location = user["location"].lower()
-        except Exception, e:
-            options.logger.error("location error: %s" % e)
-            continue
-        city = match_geoname(location)
-        if city:
-            china_map[city]['score'] += 1
-
-
 def match_location(city, location):
+    if not location:
+        return False
     if (city in location or location in city):
         return True
     else:
         return False
 
 
-@gen.coroutune
+@gen.coroutine
 def match_geoname(location):
     matched_city = None
     for city in options.city_list:
@@ -55,11 +48,16 @@ def match_geoname(location):
     if matched_city is None:
         resp = yield GeoFetch(location)
         if resp.code == 200:
-            resp = escape.json_encode(resp.body)
+            resp = escape.json_decode(resp.body)
+            options.logger.info(resp)
             for geo in resp.get("geonames", []):
                 if matched_city is None:
                     for city in options.city_list:
                         if match_location(city, geo.get("adminName1", "NoAdminName")):
                             matched_city = city
                             break
+    if matched_city:
+        options.logger.info("keyword: %s matched %s" % (location, matched_city))
+    else:
+        options.logger.info("keyword: %s can't match" % location)
     raise gen.Return(matched_city)
