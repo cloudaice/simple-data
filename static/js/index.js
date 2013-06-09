102a49c06507aa0a8f9d7a9a78619eeb09a1d77e
cloudaice <cloudaice@163.com>
2013-06-09 20:53:48 +0200

    change to jquery appendTo

--- static/js/index.js
+++ static/js/index.js
@@ -43,29 +43,34 @@ $(document).ready(function(){
     worldsocket.start();
 
     function fill_table(data, select){
-        var github_table  = "<table class='table table-striped'><thead><tr>";
-        github_table +=  "<th>Rank</th>";
-        github_table +=  "<th>Name</th>";
-        github_table +=  "<th>Score</th>";
-        github_table +=  "<th>Language</th>";
-        github_table +=  "<th>Location</th>";
-        github_table +=  "<th>Profile</th>";
-        github_table += "</tr></thead><tr>";
-        for (var i in data){
-            var count = parseInt(i) + 1;
-            github_table += "<td class='solid'>#" + count + "</td>";
-            github_table += "<td class='solid'>" + "<a href='https://github.com/" + data[i]["login"] + "' target='_blank'>" + data[i]["login"] + "</a>" + "&nbsp(" + data[i]["name"] + ")" + "</td>";
-            github_table += "<td class='solid'>" + parseInt(data[i]["score"]) + "</td>";
-            github_table += "<td class='solid'>" + data[i]["language"] + "</td>";
-            github_table += "<td class='solid'>" + data[i]["location"] + "</td>";
-            github_table += "<td class='solid'>" + "<img height='48' width='48' src=" + data[i]["gravatar"] + "/>" + "</td>";
-            github_table +="</tr><tr>";
-        }
-        github_table += "</tr><table>";
         if (data.length == 0){
             $(select).html("<div class='progress progress-striped active'><div class='bar' style='width: 40%;'></div></div>");
         }else{
-            $(select).html(github_table);
+            $(select).empty();
+            var github_table  = $("<table class='table table-striped'>");
+            github_table.appendTo(select);
+            var head = $("<thead>");
+            head.appendTo(github_table);
+            var tr = $("<tr>");
+            tr.appendTo(head);
+
+            $("<th>Rank</th>").appendTo(tr);
+            $("<th>Name</th>").appendTo(tr);
+            $("<th>Score</th>").appendTo(tr);
+            $("<th>Language</th>").appendTo(tr);
+            $("<th>Location</th>").appendTo(tr);
+            $("<th>Profile</th>").appendTo(tr);
+            for (var i in data){
+                var tr = $("<tr>");
+                tr.appendTo(github_table);
+                var count = parseInt(i) + 1;
+                $("<td class='solid'>#" + count + "</td>").appendTo(tr);
+                $("<td class='solid'>" + "<a href='https://github.com/" + data[i]["login"] + "' target='_blank'>" + data[i]["login"] + "</a>" + "&nbsp(" + data[i]["name"] + ")" + "</td>").appendTo(tr);
+                $("<td class='solid'>" + parseInt(data[i]["score"]) + "</td>").appendTo(tr);
+                $("<td class='solid'>" + data[i]["language"] + "</td>").appendTo(tr);
+                $("<td class='solid'>" + data[i]["location"] + "</td>").appendTo(tr);
+                $("<td class='solid'>" + "<img height='48' width='48' src=" + data[i]["gravatar"] + "/>" + "</td>").appendTo(tr);
+            }
         }
     }
 
